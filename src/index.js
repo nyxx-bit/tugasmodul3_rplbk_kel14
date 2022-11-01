1a4f5e0c9819b011b35ce8198d9ce9204154beda
nyxx-bit <alkhawarismi989@gmail.com>
2022-11-02 01:25:32 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import TodoContainer from './functionBased/components/TodoContainer';
 import './functionBased/App.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Router>
       <TodoContainer />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
